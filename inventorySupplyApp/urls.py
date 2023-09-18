cad3e37c97e6f192df34180f1ce8c64fac6579a6
Franz Manrique <franzmanrique2121@gmail.com>
2023-09-18 15:40:47 +0800

    updated modal view and UI

--- inventorySupplyApp/urls.py
+++ inventorySupplyApp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home),
     path('add', views.add_inventory, name='add'),
     path('edit/<int:id>', views.edit_inventory),  
-    path('view/<int:id>', views.view_inventory),  
+    path('view/<int:id>', views.view_inventory, name='view'),  
     path('update/<int:id>/', views.update_inventory, name='update'),  
     path('<int:id>/delete', views.delete_inventory, name='delete_inventory'),  
     path('inventory/', views.InventoryList.as_view()),
